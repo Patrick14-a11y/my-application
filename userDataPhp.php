b9bf91f2fbfec626647ff303ed0fa46e44fd68a2
Patrick14-a11y <116813562+Patrick14-a11y@users.noreply.github.com>
2022-11-03 20:28:11 +0800

    Add files via upload

---
+++ userDataPhp.php
@@ -0,0 +1,23 @@
+<?php
+	$Write="<?php $" . "UIDresult=''; " . "echo $" . "UIDresult;" . " ?>";
+	file_put_contents('UIDContainer.php',$Write);
+?>
+<?php
+                   include 'database.php';
+                   $pdo = Database::connect();
+                   $sql = 'SELECT * FROM table_the_iot_projects ORDER BY name ASC';
+                   foreach ($pdo->query($sql) as $row) {
+                            echo '<tr>';
+                            echo '<td>'. $row['name'] . '</td>';
+                            echo '<td>'. $row['id'] . '</td>';
+                            echo '<td>'. $row['gender'] . '</td>';
+							echo '<td>'. $row['email'] . '</td>';
+							echo '<td>'. $row['mobile'] . '</td>';
+							echo '<td><a class="btn btn-success" href="user data edit page.php?id='.$row['id'].'">Edit</a>';
+							echo ' ';
+							echo '<a class="btn btn-danger" href="user data delete page.php?id='.$row['id'].'">Delete</a>';
+							echo '</td>';
+                            echo '</tr>';
+                   }
+                   Database::disconnect();
+                  ?>
